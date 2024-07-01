c1bc746a69f76791b7f7bf3cbeea9681c0b91428
felipeorbit365 <168890234+felipeorbit365@users.noreply.github.com>
2024-07-01 19:04:39 -0300

    Exercícios da Aula 11

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,7 @@
+// Módulo D - Aula 11
+var velocidade = 78.2 
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
